10c317dd9998c1070b96b4ee234146aaefadeb1b
adrianhitosis <adrianhitosis@gmail.com>
2026-08-26 15:21:08 +0800

    you commit message

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.json());
+
     app.get('/api/health', (req, res) => {
     res.json({ status: 'Server is running smoothly' });
     });
