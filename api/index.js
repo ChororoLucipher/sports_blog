323b21658eb420a85958f16203428b778594a160
adrianhitosis <adrianhitosis@gmail.com>
2026-08-26 18:23:01 +0800

    gwapo

--- api/index.js
+++ api/index.js
@@ -6,13 +6,13 @@ app.use(express.json());
     app.get('/api/health', (req, res) => {
     res.json({ status: 'Server is running smoothly' });
     });
-
+//sample changes
 // Serve the frontend locally (Vercel serves public/ automatically in production)
 if (process.env.NODE_ENV !== 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'public')));
-  }
-
-    // Locally you need to listen; on Vercel this is ignored
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+}
+//sample code
+    /// Locally you need to listen; on Vercel this is ignored
     if (process.env.NODE_ENV !== 'production') {
     app.listen(3000, () => console.log('Local server on http://localhost:3000'));
     }
